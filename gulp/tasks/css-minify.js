4e7590a17e293e45b6d8429934224a5df18a9790
Charles Severance <csev@umich.edu>
2025-01-23 01:24:47 -0500

    Move to gulp-clean-css

--- gulp/tasks/css-minify.js
+++ gulp/tasks/css-minify.js
@@ -1,11 +1,11 @@
 const gulp      = require('gulp');
 const concat    = require('gulp-concat');
-const minifyCSS = require('gulp-minify-css');
+const cleanCSS = require('gulp-clean-css');
 const config    = require('../config').css;
 
 gulp.task('minify-css', function() {
   return gulp.src(config.src)
     .pipe(concat('breadboard.css'))
-    .pipe(minifyCSS({keepBreaks:true}))
+    .pipe(cleanCSS())
     .pipe(gulp.dest(config.dest))
 });
